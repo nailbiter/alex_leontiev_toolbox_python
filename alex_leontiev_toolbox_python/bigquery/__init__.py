eeddb9d2c0464ed39f866c7f89c1a5a4559150ed
Oleksii Leontiev <alozz1991@gmail.com>
2022-12-02 21:34:20 +0900

    robust fetch by name

--- alex_leontiev_toolbox_python/bigquery/__init__.py
+++ alex_leontiev_toolbox_python/bigquery/__init__.py
@@ -42,7 +42,9 @@ def table_exists(table_name, bq_client=None, entity="table"):
 def query_bytes(sql, bq_client=None):
     if bq_client is None:
         bq_client = bigquery.Client()
-    return bq_client.query(sql, job_config=bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)).total_bytes_processed
+    return bq_client.query(
+        sql, job_config=bigquery.QueryJobConfig(dry_run=True, use_query_cache=False)
+    ).total_bytes_processed
 
 
 def create_dataset(dataset_name, bq_client=None, exist_ok=False, location=None):
@@ -51,15 +53,14 @@ def create_dataset(dataset_name, bq_client=None, exist_ok=False, location=None):
     """
 
     # taken from https://stackoverflow.com/a/13514318
-    this_function_name = cast(
-        types.FrameType, inspect.currentframe()).f_code.co_name
+    this_function_name = cast(types.FrameType, inspect.currentframe()).f_code.co_name
     logger = logging.getLogger(__name__).getChild(this_function_name)
 
     if bq_client is None:
         bq_client = bigquery.Client()
 
     if table_exists(dataset_name, bq_client=bq_client, entity="dataset"):
-        assert exist_ok, f"dataset \"{dataset_name}\" exists"
+        assert exist_ok, f'dataset "{dataset_name}" exists'
         dataset = bq_client.get_dataset(dataset_name)
         if location is not None:
             assert dataset.location == location, (dataset.location, location)
@@ -82,7 +83,13 @@ def job_id_to_job(job_id, bq_client=None, **kwargs):
     return bq_client.get_job(job_id, **kwargs)
 
 
-def list_tables(prefix, bq_client=None, is_include_prefix=False, is_parse_dates=False, dates_fmt="%Y%m%d"):
+def list_tables(
+    prefix,
+    bq_client=None,
+    is_include_prefix=False,
+    is_parse_dates=False,
+    dates_fmt="%Y%m%d",
+):
     if bq_client is None:
         bq_client = bigquery.Client()
     prefix = prefix.split(".")
@@ -91,12 +98,15 @@ def list_tables(prefix, bq_client=None, is_include_prefix=False, is_parse_dates=
         prefix.append("")
     dataset, prefix = prefix[:-1], prefix[-1]
     dataset = ".".join(dataset)
-    tns = [t.table_id for t in bq_client.list_tables(
-        dataset) if t.table_id.startswith(prefix)]
+    tns = [
+        t.table_id
+        for t in bq_client.list_tables(dataset)
+        if t.table_id.startswith(prefix)
+    ]
     if is_include_prefix:
         tns = [f"{dataset}.{tn}" for tn in tns]
     else:
-        tns = [tn[len(prefix):] for tn in tns]
+        tns = [tn[len(prefix) :] for tn in tns]
     if is_parse_dates:
         tns = [datetime.strptime(tn, dates_fmt) for tn in tns]
     return sorted(tns)
@@ -110,10 +120,16 @@ FIXME (improve):
     1. 
 """
 TABLE_NAME_REGEX = re.compile(
-    r"""`([a-z0-9-]{5,29}[a-z0-9]\.[a-zA-Z0-9_]{1,1024}\.[\s\d\w]{1,1024})`""")
+    r"""`([a-z0-9-]{5,29}[a-z0-9]\.[a-zA-Z0-9_]{1,1024}\.[\s\d\w]{1,1024})`"""
+)
 
 
-def find_table_names_in_sql_source(sql_source, bq_client=None, is_use_bq_client=False, table_name_regex=TABLE_NAME_REGEX):
+def find_table_names_in_sql_source(
+    sql_source,
+    bq_client=None,
+    is_use_bq_client=False,
+    table_name_regex=TABLE_NAME_REGEX,
+):
     """
     FIXME:
         1. enable `is_use_bq_client`
@@ -127,3 +143,71 @@ def find_table_names_in_sql_source(sql_source, bq_client=None, is_use_bq_client=
     sql_source = sqlparse.format(sql_source, strip_comments=True)
 
     return {tn for tn in table_name_regex.findall(sql_source)}
+
+
+def table_ref_to_table(tref, proj_dataset_symbol="."):
+    return f"{tref.project}{proj_dataset_symbol}{tref.dataset_id}.{tref.table_id}"
+
+
+def _query_regex_search_method(job, dest):
+    regex = re.compile(
+        f"create\\s+(or\\s+replace\\s+)?table\\s*`{dest}`", re.IGNORECASE
+    )
+    res = regex.search(job.query) is not None
+
+    #    logging.warning((job, job.query, res, regex))
+    return res
+
+
+_FIND_JOB_BY_DESTINATION_METHODS = {
+    "job_destination": (
+        lambda job, dest: job.destination is not None
+        and table_ref_to_table(job.destination) == dest
+    ),
+    "query_regex_search": _query_regex_search_method,
+}
+
+
+def find_job_by_destination(
+    destination,
+    method="job_destination",
+    bq_client=None,
+    list_jobs_kwargs={},
+    search_timeframe=("modified", timedelta(minutes=10)),
+    custom_search_methods={},
+    is_return_multiple=False,
+    job_type="query",
+):
+    """
+    @param search_timeframe ("modified"/"created",timedelta) or (start,end) or None; search done endpoints exclusive
+    """
+    search_methods = {**_FIND_JOB_BY_DESTINATION_METHODS, **custom_search_methods}
+    assert method in search_methods, (method, list(search_methods))
+    if bq_client is None:
+        bq_client = bigquery.Client()
+
+    def_search_kwargs = {}
+    if search_timeframe is not None:
+        if search_timeframe[0] in "modified created".split():
+            attr, td = search_timeframe
+            anchor = getattr(bq_client.get_table(destination), attr)
+            start, end = anchor - td, anchor + td
+        else:
+            start, end = search_timeframe
+        def_search_kwargs = {"min_creation_time": start, "max_creation_time": end}
+    jobs = bq_client.list_jobs(
+        **{
+            **def_search_kwargs,
+            **list_jobs_kwargs,
+        }
+    )
+    jobs = [
+        job
+        for job in jobs
+        if job.job_type == job_type and search_methods[method](job, destination)
+    ]
+    if is_return_multiple:
+        return jobs
+    else:
+        (job,) = jobs
+        return job
--- tests/test___bigquery.py
+++ tests/test___bigquery.py
@@ -19,6 +19,7 @@ ORGANIZATION:
 ==============================================================================="""
 
 import alex_leontiev_toolbox_python.bigquery
+from alex_leontiev_toolbox_python.bigquery import find_job_by_destination
 from google.cloud import bigquery
 import pandas as pd
 
@@ -26,13 +27,17 @@ import pandas as pd
 def test_table_exists():
     bq_client = bigquery.Client()
     assert alex_leontiev_toolbox_python.bigquery.table_exists(
-        "bigquery-public-data.usa_names.usa_1910_2013", bq_client=bq_client)
+        "bigquery-public-data.usa_names.usa_1910_2013", bq_client=bq_client
+    )
     assert alex_leontiev_toolbox_python.bigquery.table_exists(
-        "bigquery-public-data.usa_names.usa_1910_2013")
+        "bigquery-public-data.usa_names.usa_1910_2013"
+    )
     assert alex_leontiev_toolbox_python.bigquery.table_exists(
-        "bigquery-public-data.usa_names", bq_client=bq_client, entity="dataset")
+        "bigquery-public-data.usa_names", bq_client=bq_client, entity="dataset"
+    )
     assert not alex_leontiev_toolbox_python.bigquery.table_exists(
-        "a.b.c", bq_client=bq_client)
+        "a.b.c", bq_client=bq_client
+    )
 
 
 def test_query_bytes():
@@ -42,37 +47,70 @@ def test_query_bytes():
 
 def test_job_id_to_job():
     job = alex_leontiev_toolbox_python.bigquery.job_id_to_job(
-        "38691128-02a7-4ec1-81f3-03dbca5b525d")
+        "38691128-02a7-4ec1-81f3-03dbca5b525d"
+    )
     assert job.__class__.__name__ == "QueryJob"
-    assert job.query == "select * from `api-project-424250507607.b7a150d3.t_915d8cbae22f305fab72d94e9815e391`"
+    assert (
+        job.query
+        == "select * from `api-project-424250507607.b7a150d3.t_915d8cbae22f305fab72d94e9815e391`"
+    )
 
 
 def test_find_table_names_in_sql_source():
     assert alex_leontiev_toolbox_python.bigquery.find_table_names_in_sql_source(
-        "select * from `api-project-424250507607.b7a150d3.t_915d8cbae22f305fab72d94e9815e391`") == {"api-project-424250507607.b7a150d3.t_915d8cbae22f305fab72d94e9815e391", }
-    assert alex_leontiev_toolbox_python.bigquery.find_table_names_in_sql_source(
-        """
+        "select * from `api-project-424250507607.b7a150d3.t_915d8cbae22f305fab72d94e9815e391`"
+    ) == {
+        "api-project-424250507607.b7a150d3.t_915d8cbae22f305fab72d94e9815e391",
+    }
+    assert (
+        alex_leontiev_toolbox_python.bigquery.find_table_names_in_sql_source(
+            """
         --`api-project-424250507607.b7a150d3.t_uocuoueoeu`
         select * from `api-project-424250507607.b7a150d3.t_oeroygrccyo123123`
-    """) == {"api-project-424250507607.b7a150d3.t_oeroygrccyo123123", }
+    """
+        )
+        == {
+            "api-project-424250507607.b7a150d3.t_oeroygrccyo123123",
+        }
+    )
 
 
 def test_list_tables():
-    dates = [ts.to_pydatetime() for ts in pd.date_range(
-        start="20160801", end="20170801").to_list()]
+    dates = [
+        ts.to_pydatetime()
+        for ts in pd.date_range(start="20160801", end="20170801").to_list()
+    ]
 
     res = alex_leontiev_toolbox_python.bigquery.list_tables(
-        "bigquery-public-data.google_analytics_sample.ga_sessions_")
+        "bigquery-public-data.google_analytics_sample.ga_sessions_"
+    )
     assert len(res) == len(dates)
     assert res[0] == "20160801", res[0]
 
     res = alex_leontiev_toolbox_python.bigquery.list_tables(
-        "bigquery-public-data.google_analytics_sample.ga_sessions_", is_parse_dates=True)
+        "bigquery-public-data.google_analytics_sample.ga_sessions_", is_parse_dates=True
+    )
     assert len(res) == len(dates), res
-#    assert res[0] == datetime(2016, 8, 1)
+    #    assert res[0] == datetime(2016, 8, 1)
     assert res == dates, res
 
     res = alex_leontiev_toolbox_python.bigquery.list_tables(
-        "bigquery-public-data.google_analytics_sample.ga_sessions_", is_include_prefix=True)
+        "bigquery-public-data.google_analytics_sample.ga_sessions_",
+        is_include_prefix=True,
+    )
     assert len(res) == len(dates), res
-    assert res[0] == "bigquery-public-data.google_analytics_sample.ga_sessions_20160801", res[0]
+    assert (
+        res[0] == "bigquery-public-data.google_analytics_sample.ga_sessions_20160801"
+    ), res[0]
+
+
+def test_find_job_by_destination():
+    bq_client = bigquery.Client()
+    job = find_job_by_destination(
+        f"{bq_client.project}.dataset.test_table",
+        method="query_regex_search",
+    )
+    assert (
+        job.query
+        == f"""create or replace table `{bq_client.project}.dataset.test_table` as (select 1 x)"""
+    )
