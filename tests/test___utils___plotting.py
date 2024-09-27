aa3b8277a50c0edf67ee76918c0455d93b7882a9
Oleksii Leontiev <alozz1991@gmail.com>
2024-09-27 21:51:33 +0900

    stash

--- tests/test___utils___plotting.py
+++ tests/test___utils___plotting.py
@@ -21,9 +21,39 @@ import logging
 import unittest
 from alex_leontiev_toolbox_python.utils.plotting import plot_to_pdf
 
+# from unittest.mock import MagicMock, Mock
+from unittest.mock import Mock
+import numpy as np
+import pandas as pd
+import itertools
+
 
 def test_plot_to_pdf():
-    pass
+    df = pd.DataFrame(
+        [
+            dict(p=p, r=r, c=c)
+            for p, r, c in itertools.product(range(10), range(2), range(2))
+        ]
+    )
+    df[["x", "y"]] = np.random.uniform(0, 1, (len(df), 2))
+
+    pdf = Mock()
+    plotter = Mock(return_value=None)
+
+    plot_to_pdf(
+        df,
+        plotter=plotter,
+        pages=["p"],
+        rows=["r"],
+        cols=["c"],
+        fn=pdf,
+        is_open=False,
+        # tqdm_factory=tqdm.tqdm,
+        is_loud=True,
+    )
+
+    pdf.savefig.assert_called()
+    plotter.assert_called()
 
 
 # class TestUtilsPlotting(unittest.TestCase):
