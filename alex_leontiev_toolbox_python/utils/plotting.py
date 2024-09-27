4cf9bcc004bda1af3bf06c8fc7f4b5c3b78d1a81
Oleksii Leontiev <alozz1991@gmail.com>
2024-09-27 21:41:25 +0900

    stash

--- alex_leontiev_toolbox_python/utils/plotting.py
+++ alex_leontiev_toolbox_python/utils/plotting.py
@@ -46,6 +46,16 @@ def _(x: tuple) -> list:
     return list(x)
 
 
+@functools.singledispatch
+def filename_or_pdf_to_pdf(fn):
+    return fn, False
+
+
+@filename_or_pdf_to_pdf.register
+def _(fn: str):
+    return PdfPages(fn), True
+
+
 def plot_to_pdf(
     df: pd.DataFrame,
     plotter: typing.Callable,
@@ -79,73 +89,76 @@ def plot_to_pdf(
         cols = [cn]
         df[cn] = 1
 
-    with PdfPages(fn) as pdf:
-        pl = list(df.groupby(pages))
-        for page_val, page_slice in tqdm_factory(
-            pl if page_sort_key == -1 else sorted(pl, key=page_sort_key)
+    pdf, is_created_by_us = filename_or_pdf_to_pdf(fn)
+    pl = list(df.groupby(pages))
+    for page_val, page_slice in tqdm_factory(
+        pl if page_sort_key == -1 else sorted(pl, key=page_sort_key)
+    ):
+        page_val = listify(page_val)
+        page_dict = dict(zip(pages, page_val))
+
+        distinct_row_values, distinct_col_values = (
+            _get_distinct(page_slice, rows),
+            _get_distinct(page_slice, cols),
+        )
+        if row_sort_key != -1:
+            distinct_row_values = sorted(distinct_row_values, key=row_sort_key)
+        if col_sort_key != -1:
+            distinct_col_values = sorted(distinct_col_values, key=col_sort_key)
+
+        nrows, ncols = len(distinct_row_values), len(distinct_col_values)
+
+        if is_loud:
+            logging.warning((page_dict, rows, cols, nrows, ncols))
+
+        fig, axs = plt.subplots(
+            **{"nrows": nrows, "ncols": ncols, "squeeze": False, **subplots_kwargs}
+        )
+        if is_loud:
+            logging.warning(axs)
+
+        axs_d = {}
+        for (row_val, col_val), ax in zip(
+            itertools.product(distinct_row_values, distinct_col_values),
+            axs.flatten(),
         ):
-            page_val = listify(page_val)
-            page_dict = dict(zip(pages, page_val))
+            axs_d[(tuple(row_val), tuple(col_val))] = ax
 
-            distinct_row_values, distinct_col_values = (
-                _get_distinct(page_slice, rows),
-                _get_distinct(page_slice, cols),
-            )
-            if row_sort_key != -1:
-                distinct_row_values = sorted(distinct_row_values, key=row_sort_key)
-            if col_sort_key != -1:
-                distinct_col_values = sorted(distinct_col_values, key=col_sort_key)
+        if is_loud:
+            logging.warning(axs_d)
 
-            nrows, ncols = len(distinct_row_values), len(distinct_col_values)
-
+        for v, slc in page_slice.groupby([*rows, *cols]):
+            rv, cv = tuple(v[: len(rows)]), tuple(v[len(rows) :])
             if is_loud:
-                logging.warning((page_dict, rows, cols, nrows, ncols))
-
-            fig, axs = plt.subplots(
-                **{"nrows": nrows, "ncols": ncols, "squeeze": False, **subplots_kwargs}
-            )
-            if is_loud:
-                logging.warning(axs)
-
-            axs_d = {}
-            for (row_val, col_val), ax in zip(
-                itertools.product(distinct_row_values, distinct_col_values),
-                axs.flatten(),
-            ):
-                axs_d[(tuple(row_val), tuple(col_val))] = ax
-
-            if is_loud:
-                logging.warning(axs_d)
-
-            for v, slc in page_slice.groupby([*rows, *cols]):
-                rv, cv = tuple(v[: len(rows)]), tuple(v[len(rows) :])
-                if is_loud:
-                    logging.warning((rv, cv))
-                plotter(
-                    ax=axs_d[rv, cv],
-                    row_dict=dict(zip(rows, listify(rv))),
-                    col_dict=dict(zip(cols, listify(cv))),
-                    data=slc,
-                    page_dict=page_dict,
-                )
-
-            post_process_fig_res = post_process_fig(
-                fig=fig,
+                logging.warning((rv, cv))
+            plotter(
+                ax=axs_d[rv, cv],
+                row_dict=dict(zip(rows, listify(rv))),
+                col_dict=dict(zip(cols, listify(cv))),
+                data=slc,
                 page_dict=page_dict,
-                page_slice=page_slice,
             )
 
-            post_process_fig_res = (
-                (True, True) if post_process_fig_res is None else post_process_fig_res
-            )
-            is_close, is_continue = post_process_fig_res
+        post_process_fig_res = post_process_fig(
+            fig=fig,
+            page_dict=page_dict,
+            page_slice=page_slice,
+        )
 
-            if is_close:
-                plt.close()
-            pdf.savefig(fig)
+        post_process_fig_res = (
+            (True, True) if post_process_fig_res is None else post_process_fig_res
+        )
+        is_close, is_should_continue = post_process_fig_res
 
-            if not is_should_continue:
-                break
+        if is_close:
+            plt.close()
+        pdf.savefig(fig)
+
+        if not is_should_continue:
+            break
+
+    if is_created_by_us:
+        pdf.close()
 
     if is_open:
         os.system(f"open {fn}")
---
+++ tests/test___utils___plotting.py
@@ -0,0 +1,37 @@
+"""===============================================================================
+
+        FILE: /Users/nailbiter/Documents/forgithub/alex_leontiev_toolbox_python/tests/test___utils___plotting.py
+
+       USAGE: (not intended to be directly executed)
+
+ DESCRIPTION: 
+
+     OPTIONS: ---
+REQUIREMENTS: ---
+        BUGS: ---
+       NOTES: ---
+      AUTHOR: Alex Leontiev (alozz1991@gmail.com)
+ORGANIZATION: 
+     VERSION: ---
+     CREATED: 2024-09-27T21:37:43.575304
+    REVISION: ---
+
+==============================================================================="""
+import logging
+import unittest
+from alex_leontiev_toolbox_python.utils.plotting import plot_to_pdf
+
+
+def test_plot_to_pdf():
+    pass
+
+
+# class TestUtilsPlotting(unittest.TestCase):
+#     def __init__(self, *args, **kwargs):
+#         super().__init__(*args, **kwargs)
+#         self._logger = logging.getLogger(self.__class__.__name__)
+
+#     def test_something(self):
+#         self.assertTrue(1 == 1)
+#         self.assertEqual(1, 1)
+#         self.assertNotEqual(1, 2)
