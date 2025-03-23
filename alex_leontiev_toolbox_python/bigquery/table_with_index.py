eef966d5d1996580ff0e65608740bf9d28566753
Oleksii Leontiev <alozz1991@gmail.com>
2025-03-23 14:16:58 +0900

    stash

--- alex_leontiev_toolbox_python/bigquery/table_with_index.py
+++ alex_leontiev_toolbox_python/bigquery/table_with_index.py
@@ -60,10 +60,7 @@ class _BigQuerySeries:
         )
         if self._is_column:
             df.rename(columns={"x": self._column_name}, inplace=True)
-
-        return df.set_index(
-            self._column_name if self._is_column else "x", inplace=True
-        )["cnt"]
+        return df.set_index(self._column_name if self._is_column else "x")["cnt"]
 
 
 def _table_name_or_query(s: str) -> str:
